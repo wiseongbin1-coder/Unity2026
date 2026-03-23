a3944a96a6292a9c1260e2015749bcf29f0114db
sinberowa <124888430+sinberowa@users.noreply.github.com>
2026-03-23 10:55:12 +0900

    ch03
    챕터3 마무리

--- ch03/Assets/20.Scripts/RouletteController.cs
+++ ch03/Assets/20.Scripts/RouletteController.cs
@@ -3,20 +3,26 @@ using UnityEngine;
 public class RouletteController : MonoBehaviour
 {
     float rotSpeed = 0;
+    float startspeed = 30f;
+    float decreaseRatio = 0.99f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if(Input.GetMouseButtonDown(0))
         {
-            this.rotSpeed = 10f;
+            rotSpeed = startspeed;
         }
 
-        transform.Rotate(0, 0, rotSpeed);
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        this.rotSpeed *= 0.99f;
     }
 }
