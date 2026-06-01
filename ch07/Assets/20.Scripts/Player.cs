e5cf66397d92ccbb3c9ff07079608415ee4bbacb
sinberowa <124888430+sinberowa@users.noreply.github.com>
2026-06-01 10:49:07 +0900

    ch07

--- ch07/Assets/20.Scripts/Player.cs
+++ ch07/Assets/20.Scripts/Player.cs
@@ -1,13 +1,24 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     public float movespeed = 2.0f;
+    public float rotateSpeed = 20.0f;
+    public float shootingForce = 100f;
+    public float shootingDelay = 1.5f;
+
+    public GameObject bamsongiprefab;
+    public Transform shootingPoint;
 
     Rigidbody rb;
     Animator anim;
 
     Vector3 moveDirection;
+
+    float xInput;
+    float zInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,24 +33,44 @@ public class Player : MonoBehaviour
         float xInput = Input.GetAxisRaw("Horizontal");
         float zInput = Input.GetAxisRaw("Vertical");
 
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             anim.SetTrigger("Fire");
+            Shooting();
             return;
         }
 
-        moveDirection = new Vector3 (xInput, 0, zInput);
+        moveDirection = new Vector3(xInput, 0, zInput);
 
-        if (moveDirection.magnitude > 0.1)
+        if (moveDirection.magnitude > 0.1f)
         {
             moveDirection.Normalize();
             anim.SetBool("IsWalking", true);
-
-            rb.MovePosition(rb.position + moveDirection*movespeed*Time.deltaTime);
+            Vector3 move = new Vector3(0, 0, zInput);
+            Rotate();
+            rb.MovePosition(rb.position + zInput * moveDirection * movespeed * Time.deltaTime);
         }
         else
         {
             anim.SetBool("IsWalking", false);
         }
+    }
+
+    void Rotate()
+    {
+        float rotSpeed = xInput * rotateSpeed * Time.deltaTime;
+        rb.rotation = Quaternion.Euler(0, rotSpeed, 0) * rb.rotation;
+
+    }
+
+    IEnumerator Shooting()
+    {
+        yield return new WaitForSeconds(shootingDelay);
+
+        GameObject bamsongi = Instantiate(bamsongiprefab, shootingPoint.position, shootingPoint.rotation);
+
+        Vector3 dir = shootingPoint.forward * shootingForce;
+        bamsongi.GetComponent<BamsongiController>().Shoot(dir);
+
     }
 }
--- ch07/Assets/20.Scripts/TargetPosition.cs
+++ ch07/Assets/20.Scripts/TargetPosition.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class TargetPosition : MonoBehaviour
 {
+    public float size = 1f;
+    public Color color = Color.red;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;  
+        Gizmos.color = color;  
         Gizmos.DrawSphere(transform.position, 1f);        
     }
 
